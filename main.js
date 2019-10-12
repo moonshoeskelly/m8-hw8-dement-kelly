170fac5464524ae840d36483ae88890b46391a78
Kelly DeMent <kdement@ufl.edu>
2019-10-12 15:00:56 -0600

    Add JS to toggle sub-menu on hover

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 // It is always helpful to use comments in your code!
+
+//Select Main Nav Links
+var mainNavLinks = document.getElementsByClassName('main-menu-labels');
+console.log(mainNavLinks);
+
+//Toggles hide transition
+function toggleSubMenu() {
+  this.nextElementSibling.classList.toggle('hide');
+}
+
+//Applies hide toggle on mouse over and mouse out
+for (i=0; i< mainNavLinks.length; i++) {
+  mainNavLinks[i].addEventListener('mouseover', toggleSubMenu);
+  mainNavLinks[i].addEventListener('mouseout', toggleSubMenu);
+}
