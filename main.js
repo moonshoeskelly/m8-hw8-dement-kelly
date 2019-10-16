92ba9d532563730e9dbc5a204c827f126eaa080c
Kelly DeMent <kdement@ufl.edu>
2019-10-16 01:51:06 -0600

    Try and fail to fix sub-menu visibility

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-// It is always helpful to use comments in your code!
-
 //Select Main Nav Links
+
 var mainNavLinks = document.getElementsByClassName('main-menu-labels');
 console.log(mainNavLinks);
 
@@ -9,8 +8,47 @@ function toggleSubMenu() {
   this.nextElementSibling.classList.toggle('hide');
 }
 
+function showEnter() {
+  this.nextElementSibling.classList.add('show-all');
+}
+
+function showLeave() {
+  this.nextElementSibling.classList.remove('show-all');
+}
+
 //Applies hide toggle on mouse over and mouse out
 for (i=0; i< mainNavLinks.length; i++) {
   mainNavLinks[i].addEventListener('mouseenter', toggleSubMenu);
+  mainNavLinks[i].addEventListener('mouseenter', showEnter);
   mainNavLinks[i].addEventListener('mouseleave', toggleSubMenu);
+  mainNavLinks[i].addEventListener('mouseleave', showLeave);
 }
+
+
+/*
+//Keep sub-menu displayed when hovering drop down
+var mainAll = document.querySelectorAll('.main-all');
+console.log(mainAll);
+
+function toggleDropdown() {
+  this.nextElementSibling.classList.toggle('show-all');
+}
+
+//Applies hide toggle on mouse over and mouse out
+for (i=0; i< mainAll.length; i++) {
+  mainAll[i].addEventListener('mouseenter', toggleDropdown);
+  mainAll[i].addEventListener('mouseleave', toggleDropdown);
+}
+
+/*function showEnter() {
+  mainAll.classList.add('show-all');
+}
+
+function showLeave() {
+  mainAll.classList.remove('show-all');
+}
+
+for (i=0; i< mainAll.length; i++) {
+  mainAll[i].addEventListener('mouseenter', showEnter);
+  mainAll[i].addEventListener('mouseleave', showLeave);
+}*/
